5a1aebc50ba3cad9ad866b9c6adc763edb636cee
Adonys Rauda <arauda@tribalworldwide.gt>
2022-11-11 13:09:48 -0600

    feat: added retrieve images use case

---
+++ Sources/App/Domain/UseCase/Image/RetrieveImagesUseCase.swift
@@ -0,0 +1,23 @@
+//
+//  RetrieveImagesUseCase.swift
+//  ImageApp
+//
+//  Created by Adonys Rauda on 11/11/22.
+//
+
+import Foundation
+
+final class RetrieveImagesUseCase {
+    let repository: ImageRepositoryType
+    
+    init(repository: ImageRepositoryType = ImageRepository()) {
+        self.repository = repository
+    }
+    
+    func invoke(request: ImagesRequest) async throws -> [Image] {
+        let result = try await repository.retrieveImages(imagesRequest: request)
+        let imageMapper = ImageMapper()
+        
+        return result.map { imageMapper.call(object: $0) }
+    }
+}
